e34a4d173bc01147874153a0f17b1a88ef894a8d
Azkachandam <nesta.chanda51@gmail.com>
2022-11-04 08:02:16 +0700

    Study Case modul 1

---
+++ src/Menu.java
@@ -0,0 +1,18 @@
+public class Menu {
+    public String nama;
+    public String kategori;
+    public Integer harga;
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public void setKategori(String kategori){
+        this.kategori = kategori;
+    }
+
+    public void setHarga(Integer harga){
+        this.harga = harga;
+    }
+
+    }
---
+++ src/User.java
@@ -0,0 +1,16 @@
+public class User {
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    public void username(String username){
+        System.out.println("Nama: ");
+    }
+
+    public void NIM(Integer NIM){
+        System.out.println("No. Handphone: ");
+    }
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+    public void regisuser(String regisuser){
+        System.out.println("Nama: ");
+    }
+
+
+}
